1ed9c22898d64f788f0e151828ab7f49d75e0368
pnoul <pavlos.noulis@gmail.com>
2023-01-18 17:05:39 +0200

    trying the floting-ui library

--- src/app/registration/summary/PackageSection.jsx
+++ src/app/registration/summary/PackageSection.jsx
@@ -9,67 +9,14 @@ import { ReactComponent as AddPlayerIcon } from "/assets/icons/add_player-croppe
 import { ReactComponent as CreateTeamIcon } from "/assets/icons/merge_team.svg";
 import { ReactComponent as AddPackageIcon } from "/assets/icons/add_package.svg";
 import AddPackage from "/assets/icons/add_package.svg";
-import { Tooltip, IconButton } from "@mui/material";
-
-const RoundedIcon = styled(Icon)`
-  // background-color: var(--grey-1);
-  // border-radius: 50%;
-`;
-
-const MyTooltip = styled(SvgTooltip)`
-  &:hover {
-    background-color: red;
-  }
-`;
-
-// const MyIcon = styled("div")`
-//   display: flex;
-//   width: 30px;
-//   height: 30px;
-//   background-color: var(--grey-1);
-//   border-radius: 50%;
-//   align-items: center;
-//   justify-content: center;
-// `;
-
-// const MyIcon2 = styled(Svg)`
-//   display: inline-block;
-//   padding: calc(1%);
-// `;
-
-// const MySvgBall = styled(SvgBall2)`
-//   background-color: red;
-// `;
+import { Tooltip_0 as Tooltip } from "/src/components/tooltips";
 
 function PackageSection() {
   const aRef = useRef(null);
   return (
     <Card>
       <div>this is the package section</div>
-      <RoundedIcon size="2">
-        <AddPlayerIcon />
-      </RoundedIcon>
-      <RoundedIcon size="4">
-        <CreateTeamIcon />
-      </RoundedIcon>
-      <RoundedIcon size="6">
-        <AddPackageIcon />
-      </RoundedIcon>
-      <RoundedIcon size="8">
-        <DiscountIcon />
-      </RoundedIcon>
-      <SvgBall size={5}>
-        <DiscountIcon />
-      </SvgBall>
-      <SvgButton size={5}>
-        <DiscountIcon />
-      </SvgButton>
-      <SvgTooltip size={5}>
-        <DiscountIcon />
-      </SvgTooltip>
-      <MyTooltip size={5}>
-        <DiscountIcon />
-      </MyTooltip>
+      <Tooltip></Tooltip>
     </Card>
   );
 }
---
+++ src/components/tooltips/index.js
@@ -0,0 +1,1 @@
+export * from "./Tooltip_0";
